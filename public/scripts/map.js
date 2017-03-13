7706b57970adf936685d9bcf0dd449602758f073
Rebecca Weinberger <rew@mit.edu>
2017-03-13 15:36:18 -0400

    map dot

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,10 +1,12 @@
 $(window).on('load', function() {
   $('#mapicon').on('click', function(){
+    $('#welcome').fadeOut();
     $('#overlay').fadeIn();
-    $('#map-pop').fadeIn()
+    $('#map-pop').fadeIn();
   });
   $('#closemap').on('click', function(){
     $('#overlay').fadeOut();
-    $('#map-pop').fadeOut()
+    $('#map-pop').fadeOut();
+    $('#welcome').delay(400).fadeIn();
   });
 });
